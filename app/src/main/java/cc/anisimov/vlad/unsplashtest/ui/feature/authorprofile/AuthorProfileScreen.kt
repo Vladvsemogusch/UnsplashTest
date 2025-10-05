41f4ebbcf09c4f3db1b944cfc4c84c564910ebc5
Vladyslav Anisimov <v.anisimov@monosoft.dev>
2025-10-05 12:03:38 +0300

    update compose previews

--- app/src/main/java/cc/anisimov/vlad/unsplashtest/ui/feature/authorprofile/AuthorProfileScreen.kt
+++ app/src/main/java/cc/anisimov/vlad/unsplashtest/ui/feature/authorprofile/AuthorProfileScreen.kt
@@ -7,19 +7,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,6 +23,7 @@ import cc.anisimov.vlad.unsplashtest.ui.base.UIEvent
 import cc.anisimov.vlad.unsplashtest.ui.base.component.UrlImage
 import cc.anisimov.vlad.unsplashtest.ui.base.handleEvents
 import cc.anisimov.vlad.unsplashtest.ui.base.navigation.AppGraph
+import cc.anisimov.vlad.unsplashtest.ui.feature.authorprofile.component.AuthorProfileScreenTopAppBar
 import cc.anisimov.vlad.unsplashtest.ui.theme.UnsplashTestTheme
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
@@ -109,34 +104,11 @@ private fun AuthorProfileScreen(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-private fun AuthorProfileScreenTopAppBar(onBackPress: () -> Unit) {
-    TopAppBar(
-        colors = TopAppBarDefaults.topAppBarColors()
-            .copy(containerColor = MaterialTheme.colorScheme.primary),
-        navigationIcon = {
-            IconButton(onClick = onBackPress) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_arrow_left),
-                    contentDescription = null,
-                )
-            }
-        },
-        title = {
-            Text(
-                text = stringResource(id = R.string.author_profile),
-                style = MaterialTheme.typography.displayLarge
-            )
-        }
-    )
-}
-
 @Preview
 @Composable
 private fun AuthorProfileScreenContentPreview() {
     val screenState = AuthorProfileScreenState(
-        name = "Test Name", bio = "Test Bio", profileImageUrl = ""
+        name = "John Smith", bio = "Bio text", profileImageUrl = null
     )
     
     UnsplashTestTheme {
--- app/src/main/java/cc/anisimov/vlad/unsplashtest/ui/feature/authorprofile/component/AuthorProfileScreenTopAppBar.kt
+++ app/src/main/java/cc/anisimov/vlad/unsplashtest/ui/feature/authorprofile/component/AuthorProfileScreenTopAppBar.kt
@@ -1,12 +1,14 @@
-package cc.anisimov.vlad.unsplashtest.ui.feature.imagelist.component
+package cc.anisimov.vlad.unsplashtest.ui.feature.authorprofile.component
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import cc.anisimov.vlad.unsplashtest.R
@@ -14,14 +16,21 @@ import cc.anisimov.vlad.unsplashtest.ui.theme.UnsplashTestTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ImageListScreenTopAppBar(modifier: Modifier = Modifier) {
+internal fun AuthorProfileScreenTopAppBar(onBackPress: () -> Unit) {
     TopAppBar(
-        modifier = modifier,
         colors = TopAppBarDefaults.topAppBarColors()
             .copy(containerColor = MaterialTheme.colorScheme.primary),
+        navigationIcon = {
+            IconButton(onClick = onBackPress) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_arrow_left),
+                    contentDescription = null,
+                )
+            }
+        },
         title = {
             Text(
-                text = stringResource(id = R.string.app_name),
+                text = stringResource(id = R.string.author_profile),
                 style = MaterialTheme.typography.displayLarge
             )
         }
@@ -30,8 +39,8 @@ fun ImageListScreenTopAppBar(modifier: Modifier = Modifier) {
 
 @Preview
 @Composable
-private fun ImageListScreenTopAppBarPreview() {
+private fun AuthorProfileScreenTopAppBarPreview() {
     UnsplashTestTheme {
-        ImageListScreenTopAppBar()
+        AuthorProfileScreenTopAppBar(onBackPress = {})
     }
 }
