242ff23d7bbe719ff0cf19134e6345f7517ff965
Vladyslav Anisimov <v.anisimov@monosoft.dev>
2025-11-17 19:44:41 +0200

    bump version (0.2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "cc.anisimov.vlad.unsplashtest"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = "0.2"
     }
 
     buildTypes {
